6574373636c53d91cf3f6b2a8811112f94490796
AlixKSA <alix339x@gmail.com>
2026-04-29 15:11:59 +0300

    Create source.js

---
+++ source.js
@@ -0,0 +1,72 @@
+
+"f_74", ".ac3", "_396", "9ir.", "uxa.", "_ffx", ".o7m",
+    "vy.8", ".l59", "_6yi", "_jex", "m9a_", "g8_g", "td_b",
+    "_bsx", ".txf", "jyh_", "0.4b", "mb.r", "p3_2",
+
+    "ub_z", "ty.v", "i_cy", "_qtn", "e_6i", "q4.0", "av7_",
+    "f_ge", ".sg7", "uyo_", "bw.i", "d_sa", "6s.j", "gi_v",
+    "0.rf", "71_k", ".jpr", "is_a", "6s.r", "4a.2",
+
+    ".mhf", ".wyn", "n_9z", "5.o7", "vu.s", "5sd.", "b_ar",
+    "6_j9", "e2o_", "av.o", "d6j_", "9ib.", "i.bh", "i_nc",
+    ".q95", "_yn5", "4_5u", ".x28", "o_ig", "wbq_",
+
+    "l_d0", "m.ns", "t_mq", "1p.d", "ov_n", "us.v", "v.nd",
+    "r1_k", "q.jf", "ri.f", "7u_1", "h_wi", "kl.s", "jn.o",
+    "wh_i", "qz.m", "r.ly", "i_fy", "3p.b", "c0_g",
+
+    "s_xz", "i.e7", "ks_d", ".ij4", "_0dg", "m.mw", "3.3h",
+    "9_r6", "_41x", "_pvp", "i0_k", "j.6l", "w1j_", "u70.",
+    "_wl4", "35.r", ".rnf", "0f5_", ".1kt", "8_jb", "2rk_",
+
+    "ek_j", "v1e_", "_wqb", "a_nn", "_cnh", "_30c", "_osa",
+    "a7w_", "4.a0", "_vr1", "d.39", "i_gc", "i.3t", ".gw0",
+    "_bed", "iu.l", "p.mo", "q_9b", "tu_a", "ybo.",
+
+    "e3f_", "k6h_", "_qak", "4nv.", "t_9b", "ym_f", "gpr.",
+    "dl.g", ".q7q", "x_60", "5l_n", ".jht", "e.7q", "_pwm",
+    ".0qr", "zd.k", "l.8r", "49.f", "jlb_", "hzw_",
+    "i.75", "de_w",
+
+    "9_vk", "h.lf", ".yjb", "_4je", "_4ef", "m.om", "m2u.",
+    "0z_v", "h.g0", "_xdk", "98c.", "z.x8", ".zp4", "gb_w",
+    "d.r1", "t5_e", "_d92", "14.r", ".0me", "_d5d",
+
+    "p4d.", ".yui", "_5le", "7n2.", "im.w", "7_n0", ".vxs",
+    ".pv5", "dv.l", "iz.8", "lk.r", "_3k9", "5by.", "f.85",
+    "ybs_", ".brw", "ai.4", ".srf", "rf.3", "dn.1",
+
+    "miw.", "ed.g", "_zum", "g5j_", "q.0o", "q_3p", "fjr_",
+    "sm_u", "082.", "a.vi", "gm.k", "h4b_", "cv.v", "_0vy",
+    ".ltm", "f.mo", "23q_", "o56_", "_ujp", "a.qg",
+
+    "zw.n", "z.x0", ".cdt", "f_dl", "p9_8", "a50.", "b_cj",
+    "_am6", "a_a5", "u_8r", "it.p", "5ha.", "24_d", "yrn.",
+    "k_vk", "v.ts", "4ds.", "3i_v", "eb.g", "mz.b",
+
+    # الجديدة
+    "p27.", ".c5f", "9d.b", "x_wz", "_3rv", "uw7.", ".oef",
+    "_lgs", "1.u6", "w0_h", "j5r.", "_nwo", "tn_w", "_n54",
+    "h_f0", ".1p2", ".cfo", "_m13", "3m_5", ".eyo","5_o1", "r.1g", "6.xw", "i.fg", "1e_6", "7k6_", "dsz.",
+".fb0", "mct_", "e_hs", ".7mp", "q_pd", "b.09", "b.ac",
+"cwz_", "56.4", "_qwr", ".1qr", ".lgb", "u.m6",".5j2", "1.bn", "vc_3", "_vus", ".r7i", ".3mf", "m_x9", "dt_e",
+".ebd", "1_11", ".ucj", "mr0.", "p.5c", "iz.y", "sm_a", "23.n",
+".qhd", "ex.5", "x6u.", "_jl5","0_94", "6v_d", "h5r.", ".39g", "yka_", "t_xe", ".ygg", "_fwp", "_auu",
+"ucw.", "r_2n", "_rsc", "lcb_", "oa.5", "nw_3", "x_mf", "ajg.",
+"1h_7", "ax.b", ".log", "_dfb",".zry", "a.xi", "s.r5", "a9.p", "i.lg", "_po8", "z.f2", "n_7t",
+"u0n.", "j.0l", ".m1e", "v.9g", "tp.4", "9d_0", "cx.9","1s_y", "j_y1", "io.1", "_55x", "1.17", "5_k8", "u6j.", "mv.s",
+"r8f.", "5x.8", "uqc_", "_j9z", "2ar_", "wo_i", "_cz8", "u.4o","1_vn","qdd_", "jv_u", "_yqm", ".sxy", "_310", "ih.3", "_nfn", "_d8h",
+"y_wh", "_kpe", ".pkl", "bd.h", "i3_j", "_78h", "k8m_", "_10l",
+".t22", "v.5i", "vx9_", "l4.y", ".98p", "gb.k", "qav_","3m_p", "_r29", "xjz_", ".7xl", "p.sa", "n4_n", "z.9w", "k1.z", "ft.o",
+"qq_n", "6o.x", "qo_n", "kkg_", "om_u", "486.", ".yfj", "jy_o",
+".ezd", "azs.", "3_kx", "9.el", "c.dj", "u_27", "2_zv", "q8_f",
+"gz6_", "_mgi", "o4_3", "7.bu", "01.k", "z_qc", "_fzp", ".ljg",
+"ogf_", "4ff_", "rnl_", ".ne3", "7jc_", "g21_", "1.mj", "at.g",
+"79.5", ".dav", ".c7q", "9r1.", ".6xt", "p_1y", "s3.q", "yhu_",
+"_s7c", "5c_k","1cs.", "w.5s", "p_1r", "_sgr", "r.15", "_9pc", "t.9k", ".x1p",
+"o_v6", "_z9v", ".2c1", ".xa1", "_7z6", "rco.", "sxb_", "p2y_",
+"sby.", "vl_w", "_s89", "e8.n", "vt_8", "_1hv", ".vax", "neu.",
+"c5_c", "l_bo", "_y6d", "_kww", "ye7.", "_h7t", "xff_", "sx.q",
+"hb_p", "akr.", "t.t4", "30.w", "y_c4", "_q0b", "jy.x", "y.wy",
+"_9uy", "_cxz", "u.ua", ".054", "a2z.", "rx4.", "js.f", "1f9.",
+"b7_h"
